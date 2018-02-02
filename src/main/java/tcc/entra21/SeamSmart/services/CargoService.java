5c8a19061e16180d1f0ab0ec0c3564ddeb38b2cb
gabriel-lfs <gabriel-soz@live.com>
2018-02-01 22:33:03 -0200

    Criacao dos services

--- src/main/java/tcc/entra21/SeamSmart/services/CargoService.java
+++ src/main/java/tcc/entra21/SeamSmart/services/CargoService.java
@@ -1,5 +1,10 @@
 package tcc.entra21.SeamSmart.services;
 
+import tcc.entra21.SeamSmart.entities.Cargo;
+
 public interface CargoService {
 
+	public Cargo criarCargo(String nome, Integer permissao);
+	
+	
 }
